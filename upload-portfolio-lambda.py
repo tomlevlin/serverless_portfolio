83384846d9101cacd6ce9c267903f8293a076d13
tomlevlin <tom@tleconsulting.eu>
2017-09-07 12:57:17 +0200

    Added SNS to code

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,21 +4,29 @@ import StringIO
 import zipfile
 
 def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:eu-west-1:164764295482:New-serverless-portfolio-build')
 
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    portfolio_bucket = s3.Bucket('portfolio.tle.guru')
-    build_bucket = s3.Bucket('portfoliobuild.tle.guru')
+        portfolio_bucket = s3.Bucket('portfolio.tle.guru')
+        build_bucket = s3.Bucket('portfoliobuild.tle.guru')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm)
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
-    print "New build uploaded"
+        print "New build uploaded"
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!")
+
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The portfolio was not deployed succesfully.")
+        raise
 
     return 'All done!'
