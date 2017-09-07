e70c8e6a3c6d9324327e3f85d2ac77e6ab302e01
tomlevlin <tom@tleconsulting.eu>
2017-09-07 13:36:31 +0200

    Added codepipeline function to Lambda code

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -7,14 +7,28 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:eu-west-1:164764295482:New-serverless-portfolio-build')
 
+    location = {
+        "bucketName": 'portfoliobuild.tle.guru',
+        "objectKey": 'portfoliobuild.zip'
+    }
+
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+        print "Building portfolio from " +str(location)
+
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
         portfolio_bucket = s3.Bucket('portfolio.tle.guru')
-        build_bucket = s3.Bucket('portfoliobuild.tle.guru')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -24,6 +38,10 @@ def lambda_handler(event, context):
 
         print "New build uploaded"
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!")
+
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
 
     except:
         topic.publish(Subject="Portfolio Deploy Failed", Message="The portfolio was not deployed succesfully.")
