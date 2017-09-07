2620d45f96589688a7a6015bd1300deb423a8b4c
tomlevlin <tom@tleconsulting.eu>
2017-09-07 11:49:20 +0200

    Updated to work with Lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,19 +2,23 @@ import boto3
 from botocore.client import Config
 import StringIO
 import zipfile
-import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
 
-portfolio_bucket = s3.Bucket('portfolio.tle.guru')
-build_bucket = s3.Bucket('portfoliobuild.tle.guru')
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_bucket = s3.Bucket('portfolio.tle.guru')
+    build_bucket = s3.Bucket('portfoliobuild.tle.guru')
 
-with zipfile.Zipfile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print "New build uploaded"
+
+    return 'All done!'
