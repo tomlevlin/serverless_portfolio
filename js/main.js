070f9f939ea0269f6333e0867257d182d924d0b0
tomlevlin <tom@tleconsulting.eu>
2017-09-12 12:15:07 +0200

    Added tests to build

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
-import 'react-dom'
+import React from 'react';
+import ReactDom from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+  {
+    'title': "Work Example 1",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/example1.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Work Example 2",
+    'image': {
+      'desc': "example screenshot of a project involving chemistry",
+      'src': "images/example2.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Work Example 3",
+    'image': {
+      'desc': "example screenshot of a project involving cats",
+      'src': "images/example3.png",
+      'comment': ""
+    }
+  }
+
+]
+
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
