ebefad9e5c4aa7dde7f16561bf126ae8368ca840
tomlevlin <tom@tleconsulting.eu>
2017-09-25 10:44:18 +0200

    Fixed url:s for the examples

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ import ExampleWork from './example-work';
 const myWork = [
   {
     'title': "video.tleconsulting.eu",
-    'href': "https://video.tleconsulting.eu",
+    'href': "http://video.tleconsulting.eu",
     'desc': "This is my serverless website that consists of a lot of videos taken in Spain. The site is built with the serverless architecture and consists of a process that transcodes video files to some specified formats. The site uses Auto0 for authentication and Google's firebase database for all the videos metadata.",
     'image': {
       'desc': "My serverless videosite.",
@@ -15,7 +15,7 @@ const myWork = [
   },
   {
     'title': "TLE Guru news site.",
-    'href': "http://video.tleconsulting.eu",
+    'href': "https://tle.guru",
     'desc': "This is a serverless site where all users can add their own articles for later reading. The site uses an API-Gateway for communicating with the DynamoDB database where all articles are stored. This site uses the AWS Cognito service for user authentication. ",
     'image': {
       'desc': "This is a site where you will find interesting articles.",
